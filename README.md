13092edbb80fc3ce8d7f2407ca6acc85d8bc35f2
MM1323 <mia.mcduffie.13@gmail.com>
2021-09-20 12:24:23 -0400

    Adding Assignment 1 Final Files

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # security_assignments
+
+
+## Assignment 1: Secure Messaging ##
+
+This project is due on September 17, 2021 at 5 pm. You may work with a partner on this assignment
+and submit one project per team.
+
+Secure messaging is essential to trusting any Internet communications, but it is not limited to
+commercial messaging apps or code written by professionals.
+For this assignment, you will be creating a secure 2-way messaging application using cryptographic
+primitives. At the end of the assignment, you will have a Python application that will enable you
+to exchange secure messages with anyone else in the class. No one else will be able to read these
+messages (not Colgate ITS, your ISP, other people on the same WiFi network, tech companies, etc.),
+and you will able to detect any unauthorized message modifications.
+It is a bad idea to write your own cryptograhy primitives unless you are an expert ("Don’t roll
+your own crypto!"), so you will be using the publicly available Python libraries PyCryptodome and
+pyDH.
--- assignment 01/SecureMessaging.py
+++ assignment 01/SecureMessaging.py
@@ -1,7 +1,7 @@
 """
 SecureMessaging.py
 
-NAMES: [Your full names here]
+NAMES: Mia McDuffie, Andrew Xie
 
 Run as client: python3 SecureMessaging.py [Server IP] [Server Port]
 Run as server: python3 SecureMessaging.py [Server Port]
@@ -16,12 +16,21 @@ from threading import Thread
 import Crypto
 import pyDH
 
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Hash import SHA256
 
 QUEUE_LENGTH = 1
 SEND_BUFFER_SIZE = 2048
 
 
+
 class SecureMessage:
+
+    IS_SERVER = False
+    IS_CLIENT = False
+    shared_key = 0
+    shared_key_2 = 0
 
     def __init__(self, server_ip=None, server_port=None):
         """Initialize SecureMessage object, create & connect socket,
@@ -33,12 +42,14 @@ class SecureMessage:
         # connect as client
         if server_ip and server_port:
             self.s.connect((server_ip, server_port))
+            self.IS_CLIENT = True
 
         # connect as server
         elif server_port and not server_ip:
             self.s.bind(('', server_port))
             self.s.listen(QUEUE_LENGTH)
             self.s, _ = self.s.accept()
+            self.IS_SERVER = True
 
         # Run Diffie-Hellman key exchange
         self.key_exchange()
@@ -52,7 +63,7 @@ class SecureMessage:
         """Loop to check for user input and send messages"""
         while True:
             try:
-                user_input = input().encode()
+                user_input = input().encode("ISO-8859-1")
                 sys.stdout.flush()
                 message = self.process_user_input(user_input)
                 self.s.send(message[:SEND_BUFFER_SIZE])
@@ -63,7 +74,7 @@ class SecureMessage:
     def recv_loop(self):
         """Loop to receive and print messages"""
         while True:
-            recv_msg = self.s.recv(SEND_BUFFER_SIZE).decode()
+            recv_msg = self.s.recv(SEND_BUFFER_SIZE).decode("ISO-8859-1")
             if recv_msg:
                 message = self.process_received_message(recv_msg)
                 sys.stdout.write("\t" + message + "\n")
@@ -73,15 +84,76 @@ class SecureMessage:
 
     def key_exchange(self):
         """TODO: Diffie-Hellman key exchange"""
-        pass
+
+        client_to_server = pyDH.DiffieHellman() #random number
+        server_to_client = pyDH.DiffieHellman()
+
+        cs_pubkey = client_to_server.gen_public_key() #gen a public key
+        sc_pubkey = server_to_client.gen_public_key()
+        
+
+        self.s.send((str(cs_pubkey)).encode("ISO-8859-1")[:SEND_BUFFER_SIZE]) # takes strings and turns it to bytes 
+        their_public  = int(self.s.recv(SEND_BUFFER_SIZE).decode("ISO-8859-1")) #telling how much to recieve #takes bytes and turns it to strings
+        self.shared_key = client_to_server.gen_shared_key(their_public) #[:32]
+        
+        
+        self.s.send((str(sc_pubkey)).encode("ISO-8859-1")[:SEND_BUFFER_SIZE])
+        their_public_2 = int(self.s.recv(SEND_BUFFER_SIZE).decode("ISO-8859-1"))
+        self.shared_key_2 = server_to_client.gen_shared_key(their_public_2) #[:32] #take the first 32 bytes or 16
+
 
     def process_user_input(self, user_input):
         """TODO: Add authentication and encryption"""
-        return user_input
+        if (self.IS_SERVER == True):
+            key = self.shared_key_2.encode("ISO-8859-1")
+            key = key[0:16]
+            cipher = AES.new(key, AES.MODE_EAX) 
+
+        elif (self.IS_CLIENT == True):
+            key = self.shared_key.encode("ISO-8859-1")
+            key = key[0:16]
+            cipher = AES.new(key, AES.MODE_EAX) #potentially shorten key to 16 or 32
+   
+        nonce = cipher.nonce
+        ciphertext, tag = cipher.encrypt_and_digest(user_input)
+        recv_msg = ciphertext + " ".encode("ISO-8859-1") + nonce + " ".encode("ISO-8859-1") + tag #send this message
+
+        return recv_msg #its in bytes
 
     def process_received_message(self, recv_msg):
-        """TODO: Check message integrity and decrypt"""
-        return recv_msg
+        # """TODO: Check message integrity and decrypt"""
+
+        # #unjumble recv_msg 
+        marker1 = -1
+        marker2 = -1
+
+        for x in range(len(recv_msg)):
+            if (recv_msg[x] == " "):
+                if (marker1 != -1):
+                    marker2 = x
+                else:
+                    marker1 = x
+        
+        nonce = recv_msg[marker1+1: marker2]
+        nonce = nonce.encode("ISO-8859-1")
+        # # #Decryption
+        
+        if (self.IS_SERVER == True):
+            key = self.shared_key.encode("ISO-8859-1") #turn into bites
+            key = key[0:16] #first 16 bytes
+            cipher = AES.new(key, AES.MODE_EAX, nonce)
+        elif (self.IS_CLIENT == True):
+            key = self.shared_key_2.encode("ISO-8859-1")
+            key = key[0:16]
+            cipher = AES.new(key, AES.MODE_EAX, nonce)
+        plaintext = cipher.decrypt(recv_msg[0: marker1].encode("ISO-8859-1"))
+
+        try:
+            cipher.verify(recv_msg[marker2+1:].encode("ISO-8859-1"))
+        except ValueError:
+            print("MessageModificationDetected")   
+
+        return plaintext.decode("ISO-8859-1") 
 
 
 def main():
@@ -109,3 +181,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
